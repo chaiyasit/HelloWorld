a7f6f80793b6637a5f855d362fdf8de3fee57b48
chaiyasit <thumketanan@gmail.com>
2011-07-02 11:04:00 -0700

    Change manual

--- HelloWorld/src/HelloWorld0.java
+++ HelloWorld/src/HelloWorld0.java
@@ -1,9 +1,9 @@
-
 public class HelloWorld0 {
 		public static void main(String[] args) {
 			System.out.println("Hello World");
 			System.out.println("Hello World");
 			System.out.println("Hello World");
 			System.out.println("Hello World");
+                            System.out.println("Hello World");
 		}
 }
