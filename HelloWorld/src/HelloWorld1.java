6b8a4c5ad0d5b3b9d136a9de76559a9eb2b7c6ad
chaiyasit <thumketanan@gmail.com>
2011-07-03 03:22:22 +0700

    q

--- HelloWorld/src/HelloWorld1.java
+++ HelloWorld/src/HelloWorld1.java
@@ -2,5 +2,7 @@
 public class HelloWorld1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World1");
+		System.out.println("Hello World1");
+		System.out.println("Hello World1");
 	}
 }
